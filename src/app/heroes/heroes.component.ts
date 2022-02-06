77ab7fb6109006e0d630ca9a6beeb90cb0b311e6
Rahul joshi <joshirahul329@gmail.com>
2022-02-06 15:56:32 +0530

    Added routing, Dashboard component..etc

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,11 +9,11 @@ import { MessageService } from '../message.service';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
+
 export class HeroesComponent implements OnInit {
 
   heroes: Hero[] = [];
-
-  selectedHero?: Hero;
 
   constructor(private heroService: HeroService, private messageService: MessageService) { }
 
@@ -21,13 +21,8 @@ export class HeroesComponent implements OnInit {
     this.getHeroes();
   }
 
-
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
-  }
 }
